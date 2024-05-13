4e0b23c03dc9a9f542d66622da567b7a3f208c2e
Spacefox95 <nathan.raynal@gmail.com>
2024-05-13 18:26:44 +0200

    Request_name

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require("request");
 const movie_id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
 request.get(url, (error, response, body) => {
-	if (error) {
-		console.error('Error', error);
-	} else {
-		const movie_name = JSON.parse(body);
-		console.log(movie_name.title);
-	}
+  if (error) {
+    console.error('Error', error);
+  } else {
+    const movie_name = JSON.parse(body);
+    console.log(movie_name.title);
+  }
 });
