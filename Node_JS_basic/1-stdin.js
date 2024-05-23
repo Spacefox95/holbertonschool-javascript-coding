5a36ec6453a8c4b6fbde5e6b94f1c151d153ec3d
Spacefox95 <nathan.raynal@gmail.com>
2024-05-23 09:53:45 +0200

    Stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+});
+rl.on('SIGINT', () => {
+  console.log('This important software is now closing\n');
+  process.exit();
+});
