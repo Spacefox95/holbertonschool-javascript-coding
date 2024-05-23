0ecb21c12ae46b9fabfcc0cba218b2e951583290
Spacefox95 <nathan.raynal@gmail.com>
2024-05-23 09:58:40 +0200

    Mmh

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
 });
-rl.on('SIGINT', () => {
-  console.log('This important software is now closing\n');
+rl.on('close', () => {
+  console.log('This important software is now closing');
   process.exit();
 });
